309c008d18c9480427fafc741e5745cd7e74fb52
LD-BTS-SIO <122521972+LD-BTS-SIO@users.noreply.github.com>
2024-04-22 11:52:57 +0200

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,11 @@
-export const environment = {
+/*export const environment = {
  apiGetBoxes: "https://ldv-sushi-api.azurewebsites.net/api/boxes",
  apiGetImages: "https://ldv-sushi-api.azurewebsites.net/api/image"
         
-};
+};*/
+
+export const environment = {
+    apiGetBoxes: "https://localhost:8080/api/boxes",
+    apiGetImages: "https://localhost:8080/api/image"
+           
+   };
